502c65b6843be45e272065f6445e81a5c0dfe7f5
mabdolali <maryam.abdolali@gmail.com>
2024-03-22 13:20:02 +0330

    update results

--- experiments/paper_noisy.m
+++ experiments/paper_noisy.m
@@ -9,9 +9,9 @@ set(0, 'DefaultLineLineWidth', 2);
 
 
 %% generate data & initialization
-m = 3; % dimension
+m = 4; % dimension
 r = m; % # of endmembers
-SNR = 40;
+SNR = 60;
 num_experiments=10; % # of trials
 startp = (1/(r-1)+0.01); % starting purity value
 endp = 1; % ending purity value
