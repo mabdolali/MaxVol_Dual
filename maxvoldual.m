1196477f16f85d01f1da2df9822cfe7e0140e9b2
Nicolas Gillis <nicolasgillis@gmail.com>
2024-03-22 14:55:16 +0100

    Update maxvoldual.m: modify a few minor things

--- maxvoldual.m
+++ maxvoldual.m
@@ -1,7 +1,7 @@
 %   This code solves the maxvol-dual problem
 %
-%        max_{Z,Theta,Delta} det(Z)^2 - lambda ||Delta||^2 
-%                      such that Z = [Theta; e'] and Y' Theta <= 1 + Delta 
+%        max_{Z,Theta,Delta} det(Z)^2 - lambda ||Delta||^2
+%                      such that Z = [Theta; e'] and Y' Theta <= 1 + Delta
 %
 % ****** Input ******
 % X      :  the input matrix
@@ -15,7 +15,7 @@
 % .num_workers: number of parallelized solutions used
 %             -
 % ****** Output ******
-% 
+%
 % v1          :    estimated center vector
 % West        :    estimated W
 % best_theta  :    final Theta at the convergence with maximum dual volume
@@ -39,20 +39,18 @@ end
 % pre-processing
 MAX =  max(max(X));
 X = X / MAX ;
-MEAN = mean(X,2);
-v = MEAN;
+v = mean(X,2);
 
 if issparse(X)
-   [m,n]=size(X); %if X is sparse, compute svds implicitly
-   [C,~,~]=svds(@afun,[m,n],r-1); 
+    [m,n]=size(X); %if X is sparse, compute svds implicitly
+    [C,~,~]=svds(@afun,[m,n],r-1);
 else
-   Y = X - v;
-   [C,~,~]=svds(Y,r-1);
+    Y = X - v;
+    [C,~,~]=svds(Y,r-1);
 end
 % initialization
 iter = 0;
-v1 = ones(size(MEAN));
-v1 = v1 / norm(v1,'fro');
+v1 = 0;
 nn = 0;
 theta = cell(options.num_workers,1);
 ignore = cell(options.num_workers,1);
@@ -62,11 +60,12 @@ z = cell(options.num_workers,1);
 for i = 1: options.num_workers
     z{i}=[randn(r-1,r);ones(1,r)];
 end
+CtX = C'*X;
 % main loop
-while norm(v-v1,'fro')/norm(v1,'fro') > options.epsilon && iter < options.maxiter 
+while norm(v-v1,'fro')/norm(v1,'fro') > options.epsilon && iter < options.maxiter
     % projection
     v1 = v;
-    Y = C'*X - C'*v;
+    Y = CtX - C'*v;
     iter = iter + 1;
     % parallel computation of Z_i for i = 1: num_workers
     parfor i=1:options.num_workers
@@ -76,46 +75,47 @@ while norm(v-v1,'fro')/norm(v1,'fro') > options.epsilon && iter < options.maxite
     % select the best candidate till now (maximum dual volume)
     best_theta = [];
     best = 0;
-        for i = 1: options.num_workers
-            if ~ignore{i}
-                vol = (det(z{i}))^2-lambda*norm(delta{i},'fro');
-                if vol > best
-                    best_theta = theta{i};
-                    best = vol;
-                end
-            else
-                z{i}=[randn(r-1,r);ones(1,r)];
+    for i = 1: options.num_workers
+        if ~ignore{i}
+            vol = (det(z{i}))^2-lambda*norm(delta{i},'fro');
+            if vol > best
+                best_theta = theta{i};
+                best = vol;
             end
-        end
-     % if all candidates failed, use alternative initialization
-        if isempty(best_theta)
-            nn = nn + 1;
-            [J,~] = SNPA(X,nn*r);
-            v = mean(X(:,J),2);
-            if issparse(X)
-               [m,n]=size(X); %if X is sparse, compute svds implicitly
-               [C,~,~]=svds(@afun,[m,n],r-1); 
-            else
-               Y = X - v;
-               [C,~,~]=svds(Y,r-1);
-            end
-            v1=randn(r,1);
         else
-      % find intersections (W)
-            W_e = [];
-            cro=nchoosek(1:r,r-1); % for each r-1 facets from r facets
-            for i=1:size(cro,1)
-                c=cro(i,:);
-                U = best_theta(:,c);
-                O = ones(r-1,1);
-                coef=U' \ O;
-                W_e=[W_e coef];
-            end
-       % update mean vector
-            W2 = W_e;
-            West = C * W_e + v;
-            v = mean(West,2);
+            z{i}=[randn(r-1,r);ones(1,r)];
         end
+    end
+    % if all candidates failed, use alternative initialization
+    if isempty(best_theta)
+        nn = nn + 1;
+        [J,~] = SNPA(X,nn*r);
+        v = mean(X(:,J),2);
+        if issparse(X)
+            [m,n]=size(X); %if X is sparse, compute svds implicitly
+            [C,~,~]=svds(@afun,[m,n],r-1);
+        else
+            Y = X - v;
+            [C,~,~]=svds(Y,r-1);
+        end
+        CtX = C'*X; 
+        v1 = 0;
+    else
+        % find intersections (W)
+        W_e = [];
+        cro=nchoosek(1:r,r-1); % for each r-1 facets from r facets
+        for i=1:size(cro,1)
+            c=cro(i,:);
+            U = best_theta(:,c);
+            O = ones(r-1,1);
+            coef=U' \ O;
+            W_e=[W_e coef];
+        end
+        % update mean vector
+        W2 = W_e;
+        West = C * W_e + v;
+        v = mean(West,2);
+    end
 end
 West = West * MAX;
 
@@ -124,14 +124,11 @@ function Ax = afun(x, cond)
     % matrix. The function afun satisfies these required conditions:
     % Afun(x,'notransp') accepts a vector x and returns the product A*x.
     % Afun(x,'transp') accepts a vector x and returns the product A'*x.
-    
+
     if strcmp(cond,'notransp')
-    Ax = X * x - v * x;
+        Ax = X * x - v * x;
     else
-    Ax = X' * x - v' * x;
+        Ax = X' * x - v' * x;
     end
 end
 end
-
-
-
